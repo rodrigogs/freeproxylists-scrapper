87a43d1f71f963a23f8142f521dde9d85e06173c
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2016-01-20 18:16:57 +0000

    fix

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ module.exports = {
                                     }
                                     /* XXX */
                                     , gateways => {
-                                        gateways = gateways
+                                        gateways = (gateways || [])
                                             .filter(n => {
                                                 return n
                                                     && n.hostname
